a124e6283ba95ed1196783cd180afd4c1b9ea384
forest611 <forest611328@gmail.com>
2018-10-26 20:34:10 +0900

    基礎部分完成 csv取得,自動更新 実装予定

--- mce for desktop/MainWindow.xaml.cs
+++ mce for desktop/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace mce_for_desktop
 {
@@ -20,9 +21,93 @@ namespace mce_for_desktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> buttonName = new List<string>();
+        List<Button> buttons = new List<Button>();
+        static Dictionary<int, string[]> itemData = new Dictionary<int, string[]>();
+        int size = 60;
+        int delay = 5000;
+        static int id = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            getCSV();
+            setButton();
+
+            //時計処理
+            DispatcherTimer timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) };
+            timer.Tick += (s, ex) => { time.Content = DateTime.Now.ToString("h:mm:ss"); };
+            timer.Start();
+
+            Task.Run(() =>
+            {
+                getCSV();
+                updateButtonContent();
+                Console.WriteLine("task");
+                Task.Delay(delay);
+            });
+
+        }
+
+
+
+        private void setButton()
+        {
+            for(int i = 0;i!= size; i++)
+            {
+                buttonName.Add("button"+i);//list
+                Button btn = new Button();
+                btn.Height = 64;
+                btn.Name = buttonName[i];//x:NAME
+                btn.Click += Button_Click;
+                btn.Content = itemData[i][0] + Environment.NewLine + itemData[i][1];
+                panel.Children.Add(btn);
+            }
+        }
+
+        private void getCSV()
+        {
+            itemData.Clear();
+            for(int i = 0;i!=size; i++)
+            {
+                try
+                {
+                    //utf-8でcsvを読み込む 取得したデータを返す
+                    //取得できなかった場合、 catchし、取得できないと返す
+                    var csv = new System.IO.StreamReader($"http://man10.red/mce/{i}/index.csv", System.Text.Encoding.GetEncoding("UTF-8"));
+                    using (csv) ;
+                    itemData.Add(i, csv.ReadLine().Split(new string[] { " ", "　" }, StringSplitOptions.RemoveEmptyEntries));
+
+                }
+                catch (System.Exception e)
+                {
+                    Console.WriteLine("csvを開くことができませんでした" + e.Message);
+                    Console.WriteLine($"http://man10.red/mce/{i}/index.csv");
+                    itemData.Add(i, new string[] { "取得不可", "$0", "" });
+                }
+
+
+            }
+        }
+
+        private void updateButtonContent()
+        {
+            for(int i = 0; i != buttons.Count(); i++)
+            {
+                buttons[i].Content = itemData[i][0] + Environment.NewLine + itemData[i][1];
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            id = buttonName.IndexOf(((Button)sender).Name);
+            data.Content = itemData[id][0] + "        " + itemData[id][1];
+
+        }
+
+        private void update(object sender, RoutedEventArgs e)
+        {
+            data.Content = itemData[id][0] + "        " + itemData[id][1];
         }
     }
 }
